288d8aece0cfa7c8e97dc2726bf85f3cf948e67f
Kang YuanYuan <e1062196@u.nus.edu>
2025-09-01 23:31:00 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/NoNo.java
@@ -0,0 +1,7 @@
+public class NoNo {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm NoNo");
+        System.out.println("What can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
