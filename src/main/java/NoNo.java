a49019114b0bb72edb56c5a3be73924b09b987eb
Kang YuanYuan <e1062196@u.nus.edu>
2025-09-02 21:02:33 +0800

    Level 1. Echo

--- src/main/java/NoNo.java
+++ src/main/java/NoNo.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class NoNo {
     public static void main(String[] args) {
         System.out.println("Hello! I'm NoNo");
         System.out.println("What can I do for you?\n");
+
+        String instruction;
+        Scanner in = new Scanner(System.in);
+        instruction = in.nextLine();
+        while (!instruction.equals("bye")){
+            System.out.println(instruction + "\n");
+            instruction = in.nextLine(); // update instruction
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
