3aa052a499fe39dba35d543f1d555ded99b88fb9
Kang YuanYuan <e1062196@u.nus.edu>
2025-09-02 22:17:26 +0800

    Level 2. Add, List

--- src/main/java/NoNo.java
+++ src/main/java/NoNo.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class NoNo {
@@ -6,10 +7,25 @@ public class NoNo {
         System.out.println("What can I do for you?\n");
 
         String instruction;
+        String[] taskList = new String[100];
         Scanner in = new Scanner(System.in);
         instruction = in.nextLine();
+        int i = 0;
         while (!instruction.equals("bye")){
-            System.out.println(instruction + "\n");
+            if (instruction.equals("list")) {
+                System.out.println("to do list: ");
+                if (i == 0) {
+                    System.out.println("no task added yet\n");
+                } else {
+                    for (int j = 0; j < i; j++) {
+                        System.out.println((j+1) + ". " + taskList[j] + "\n");
+                    }
+                }
+            } else {
+                taskList[i] = instruction;
+                System.out.println("new task added: " + instruction + "\n");
+                i++;
+            }
             instruction = in.nextLine(); // update instruction
         }
         System.out.println("Bye. Hope to see you again soon!");
