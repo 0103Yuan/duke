48ea2824741faa63d5ede611658eca88a1c1b7e8
Kang YuanYuan <e1062196@u.nus.edu>
2025-09-15 19:41:28 +0800

    mark as done / not done, add task class

--- src/main/java/NoNo.java
+++ src/main/java/NoNo.java
@@ -6,27 +6,58 @@ public class NoNo {
         System.out.println("Hello! I'm NoNo");
         System.out.println("What can I do for you?\n");
 
-        String instruction;
-        String[] taskList = new String[100];
+        Task[] taskList = new Task[100];
+        int taskNum = 0;
+
         Scanner in = new Scanner(System.in);
-        instruction = in.nextLine();
-        int i = 0;
+        String instruction = in.nextLine();
+
         while (!instruction.equals("bye")){
-            if (instruction.equals("list")) {
-                System.out.println("to do list: ");
-                if (i == 0) {
-                    System.out.println("no task added yet\n");
-                } else {
-                    for (int j = 0; j < i; j++) {
-                        System.out.println((j+1) + ". " + taskList[j] + "\n");
+            if (instruction.startsWith("mark")) {
+                String[] words = instruction.split(" ");
+                if (words.length == 2) {
+                    int numDone = Integer.parseInt(words[1]) - 1; // convert string to num
+                    if (numDone < 0 || numDone >= taskNum ) {
+                        System.out.println("Invalid task number\n");
+                        instruction = in.nextLine(); // next instruction
+                        continue;
+                    } else {
+                        taskList[numDone].markAsDone(true);
+                        System.out.println("Nice! The task is marked as done:");
+                        System.out.println("[" + taskList[numDone].getStatusIcon() + "] " + taskList[numDone] + "\n");
                     }
                 }
+            } else if (instruction.startsWith("unmark")) {
+                String[] words = instruction.split(" ");
+                if (words.length == 2) {
+                    int numDone = Integer.parseInt(words[1]) - 1;
+                    if (numDone < 0 || numDone >= taskNum ) {
+                        System.out.println("Invalid task number\n");
+                        instruction = in.nextLine(); // next instruction
+                        continue;
+                    } else {
+                        taskList[numDone].markAsDone(false);
+                        System.out.println("OK! The task is marked as not done yet:");
+                        System.out.println("[" + taskList[numDone].getStatusIcon() + "] " + taskList[numDone] + "\n");
+                    }
+                }
+            } else if (instruction.equals("list")) {
+                System.out.println("to do list: ");
+                if (taskNum == 0) {
+                    System.out.println("no task added yet\n");
+                } else {
+                    for (int j = 0; j < taskNum; j++) {
+                        System.out.println((j+1) + ". " + "[" + taskList[j].getStatusIcon() + "]" + taskList[j]);
+                    }
+                    System.out.println();
+                }
             } else {
-                taskList[i] = instruction;
-                System.out.println("new task added: " + instruction + "\n");
-                i++;
+                Task t = new Task(instruction);
+                taskList[taskNum] = t;
+                System.out.println("new task added: " + t + "\n");
+                taskNum++;
             }
-            instruction = in.nextLine(); // update instruction
+            instruction = in.nextLine(); // next instruction
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
